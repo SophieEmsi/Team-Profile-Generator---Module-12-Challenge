72a547d123c932981a285e5cb5f27bd16eace692
Sophie McNally <soph_mcnally@msn.com>
2023-05-15 16:13:05 +0100

    adding intern questions to index.js

--- starter/index.js
+++ starter/index.js
@@ -59,7 +59,28 @@ const managerQuestions = [
       message: "What is the engineer's GitHub username?",
     },
   ];
-  
+  const internQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the intern's name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the intern's employee ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the intern's email address?",
+    },
+    {
+      type: "input",
+      name: "school",
+      message: "What school does the intern attend?",
+    },
+  ];
   
   inquirer
     .prompt(questions)
